82f7c521ad61f838e46a24876c2d55fc19204a81
Faraz Rasheed <faraz.itpk@gmail.com>
2026-05-11 01:20:41 +0500

    Delete Sudoku Game directory

--- Sudoku Game/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            SudokuFrame frame = new SudokuFrame();
-            frame.setVisible(true);
-        });
-    }
-}
